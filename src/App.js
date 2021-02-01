6e0391e19979afaf01e98c1ae6af4f23feda7cdc
Joseph M <jmedina1985@hotmail.com>
2021-01-31 23:19:50 -0800

    Getting started - 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Sidebar />
+    </>
   );
 }
 
