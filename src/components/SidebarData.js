97fe5139fd888c6da609bac3ad08baa8295f71dd
Joseph M <jmedina1985@hotmail.com>
2021-01-31 23:35:22 -0800

    SidebarData - 3

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -3,3 +3,13 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+  {
+    title: 'Overview',
+    path: '/overview',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  },
+];
