cae147fd834ae0588bcdcf8b7965cfaf6f72c016
Joseph M <jmedina1985@hotmail.com>
2021-02-01 22:42:04 -0800

    Working on Sidebar - 2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,28 @@ import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 
+const Nav = styled.div`
+  background: #15171c;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+`;
+
 const Sidebar = () => {
-  return <div>Sidebar!</div>;
+  return (
+    <>
+      <Nav>
+        <NavIcon to="#">
+          <FaIcons.FaBars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
 };
 
 export default Sidebar;
