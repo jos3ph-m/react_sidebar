c5a15c76f0b628cc2dc3b07efb8f3aeb6ff06811
Joseph M <jmedina1985@hotmail.com>
2021-02-01 22:35:40 -0800

    Working on Sidebar - 1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
 const Sidebar = () => {
   return <div>Sidebar!</div>;
