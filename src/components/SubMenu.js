1036717b9f1ca55ca461813662214e26c4518067
Joseph M <jmedina1985@hotmail.com>
2021-02-02 22:55:42 -0800

    SubMenu - 3

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #e1e9fc;
+`;
+
 const SubMenu = () => {
   return (
     <>
-      <SidebarLink></SidebarLink>
+      <SidebarLink to={item.path}></SidebarLink>
     </>
   );
 };
