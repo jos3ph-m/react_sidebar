b2070587e667586988ac0944e9012046011e744c
Joseph M <jmedina1985@hotmail.com>
2021-02-03 13:13:33 -0800

    Pages - 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
       <Sidebar />
       <Switch>
         <Route path="/overview" exact component={Overview} />
+        <Route path="/reports" exact component={Reports} />
+        <Route path="/reports/reports1" exact component={ReportsOne} />
+        <Route path="/reports/reports2" exact component={ReportsTwo} />
+        <Route path="/reports/reports3" exact component={ReportsThree} />
       </Switch>
     </Router>
   );
