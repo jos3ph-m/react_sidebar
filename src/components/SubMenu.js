9eef2c8d4ab3726afe6ff1df0f000dbef12afe12
Joseph M <jmedina1985@hotmail.com>
2021-02-02 22:54:10 -0800

    SubMenu - 2

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const SubMenu = () => {
+  return (
+    <>
+      <SidebarLink></SidebarLink>
+    </>
+  );
+};
+
+export default SubMenu;
