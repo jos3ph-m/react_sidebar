b826f0269310003868c64baad8c8d4d1a32e952f
Joseph M <jmedina1985@hotmail.com>
2021-02-02 23:03:02 -0800

    SubMenu - 6

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -5,9 +5,24 @@ import styled from 'styled-components';
 const SidebarLink = styled(Link)`
   display: flex;
   color: #e1e9fc;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+
+  &:hover {
+    background: #252831;
+    border-left: 4px solid #632ce4;
+    cursor: pointer;
+  }
 `;
 
-const SidebarLabel = styled.span``;
+const SidebarLabel = styled.span`
+  margin-left: 16px;
+`;
 
 const SubMenu = ({ item }) => {
   return (
